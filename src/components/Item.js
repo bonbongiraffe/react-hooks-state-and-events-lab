90813e7db68f12f5bd7be16b472eccb2a88cf0f5
bonbongiraffe <francescowai@yahoo.com>
2023-06-13 13:23:13 -0400

    first attempt

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  let [inCart, cartSetter] = useState(0)
+  const itemClass = inCart ? "in-cart" : ""
+  const buttonText = inCart ? "Remove From Cart" : "Add to Cart"
+  function toggleCart(){
+    cartSetter(inCart = !inCart)
+  }
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={toggleCart} className="add">{buttonText}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  let [selectedCategory, filterSetter] = useState("All")
+  console.log(selectedCategory)
+  function updateFilter(searchVal){
+    filterSetter(selectedCategory = searchVal)
+  }
+  const renderAll = items.map((item) => (
+    <Item key={item.id} name={item.name} category={item.category} />
+  ))
+  const renderFiltered = items.filter((item) => (item.category === selectedCategory))
+  .map((item) => (
+    <Item key={item.id} name={item.name} category={item.category} />
+  ))
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select onChange={(e) => updateFilter(e.target.value)} name="filter">
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,9 +26,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+        {(selectedCategory === "All" ? renderAll : renderFiltered)}        
       </ul>
     </div>
   );
